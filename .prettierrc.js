39b381115ee24c31fe2844fb3f6b5ec64297c5d2
Christoffer Carlsson <cc@christofferc.com>
2021-08-12 17:02:41 +0200

    Wrap prose if it exceeds the print width

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  jsxBracketSameLine: true,
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false,
+  proseWrap: 'always'
+};
